054f919284957d14ac9eadd476b4d7ab145b2d22
jameswilddev <jameswilddev@outlook.com>
2022-05-25 22:26:46 +0100

    Ensure that classes from plugins are usable from the application.

--- tests/first-run/expected/ephemeral/intermediate/index.js
+++ tests/first-run/expected/ephemeral/intermediate/index.js
@@ -1,4 +1,4 @@
 "use strict";
+const fromOtherPlugin = `Test From Other Plugin`;
 const fromSrc = `Test From Intermediate`;
 const fromIntermediate = `Test From Src`;
-const fromOtherPlugin = `Test From Other Plugin`;

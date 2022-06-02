324efc9430d42a9cfd81a6e8d70d9e8d8ee4e955
jameswilddev <jameswilddev@outlook.com>
2022-06-02 17:58:52 +0100

    Fix inconsistent order.

--- generate-tsconfig.js
+++ generate-tsconfig.js
@@ -3,5 +3,5 @@ const path = require(`path`);
 
 fs.writeFileSync(`./plugins/type-script/tsconfig.json`, JSON.stringify({
   extends: `../../tsconfig.json`,
-  files: process.argv.slice(2).map(line => path.join(`..`, `..`, line)),
+  files: process.argv.slice(2).map(line => path.join(`..`, `..`, line)).sort(),
 }));
